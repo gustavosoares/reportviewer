3360f4d1356094d0a077a2672cabe6471779597c
Gustavo Soares <gustavosoares@gmail.com>
2009-08-25 14:12:06 -0300

    definicao de paths

---
+++ puppet/parser/config.py
@@ -0,0 +1,12 @@
+
+# where your reports are stored, same as reportdir in puppet.conf
+REPORTDIR = "/opt/htdocs-puppet/puppetreports"
+
+# where your rrd files are stored, same as rrddir in puppet.conf
+RRDDIR = "/opt/htdocs-puppet/rrd"
+
+# where to find the rrd's on the web, can be full url or relative
+RRDROOT = "/rrd"
+
+# where the puppetmaster yaml directory is
+YAMLDIR = "/var/lib/puppet/yaml"
