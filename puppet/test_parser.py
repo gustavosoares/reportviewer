ae9f18710d6f6d61a1440ba9da762bb7db9349e9
Gustavo Soares <gustavosoares@gmail.com>
2009-09-14 11:46:45 -0300

    testes de performance com o listdir e o popen ls

--- puppet/test_parser.py
+++ puppet/test_parser.py
@@ -1,4 +1,4 @@
-
+import os
 #from parser.reports import parse_report
 from parser.util import *
 #from parser.puppethost import puppetHost
@@ -6,8 +6,27 @@ from parser.util import *
 
 #As definicoes do diretorios sao obtidas pelo import parser.config
 
+print 'list dir performance'
+inicio = start_counter()
+yamlfiles = os.listdir('/opt/puppet/reports/riovld46.globoi.com/')
+yamlfiles.sort()
+elapsed(inicio)
+print '*' * 60
+print ''
+print 'ls performance'
+inicio = start_counter()
+yamlfiles_ls = os.popen('ls /opt/puppet/reports/riovld46.globoi.com/').readlines()
+elapsed(inicio)
+print '*' * 60
+print ''
+print yamlfiles_ls
+
 
 yamlfile = '/opt/puppet/reports/riovld46.globoi.com/200909010020.yaml'
 #yamlfile = '/tmp/teste.yaml'
-yaml = load_yaml(yamlfile)
-print yaml
+#yaml = load_yaml(yamlfile)
+
+
+
+
+
